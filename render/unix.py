672d50ff1d0c864eecda2f437519aa511025e1cd
Liplum <Li_plum@outlook.com>
2022-10-30 11:42:38 +0800

    Windows console canvas

---
+++ render/unix.py
@@ -0,0 +1,1 @@
+def mix_color(fg: int, bk: int) -> int: return bk | (fg << 3)
---
+++ render/win.py
@@ -0,0 +1,132 @@
+from typing import Iterator, Optional
+
+import numpy as np
+import win32con
+import win32console
+from win32console import PyConsoleScreenBufferType, PyCOORDType
+
+from .canvas import Renderer, Canvas
+
+"""
+|---x--------->
+| 0 1 2 3 4 5 6
+y 1 2 3 4 5 6 7
+| 2 3 4 5 6 7 8
+v 3 4 5 6 7 8 9
+"""
+
+
+def mix_color(fg: int, bk: int) -> int: return fg | bk
+
+
+class WinCanvas(Canvas):
+
+    def __init__(self, width: int, height: int):
+        self._sizeX = width
+        self._sizeY = height
+        self.char: np.ndarray | None = None
+        self.fg: np.ndarray | None = None
+        self.bg: np.ndarray | None = None
+        self.dirty = False
+
+    def setChar(self, x: int, y: int, char: str):
+        sizeX = self.sizeX
+        sizeY = self.sizeY
+        if 0 <= x < sizeX and 0 <= y < sizeY:
+            self.char[x * sizeY + y] = char
+            self.dirty = True
+
+    def setColor(self, x: int, y: int, *, fg: int = None, bg: int = None):
+        sizeX = self.sizeX
+        sizeY = self.sizeY
+        if 0 <= x < sizeX and 0 <= y < sizeY:
+            if fg is not None:
+                self.fg[x * sizeY + y] = fg
+                self.dirty = True
+            if bg is not None:
+                self.bg[x * sizeY + y] = bg
+                self.dirty = True
+
+    def getColor(self, x: int, y: int) -> tuple[int, int] | None:
+        sizeX = self.sizeX
+        sizeY = self.sizeY
+        if 0 <= x < sizeX and 0 <= y < sizeY:
+            return self.fg[x * sizeY + y], self.bg[x * sizeY + y]
+        return None
+
+    def getChar(self, x: int, y: int) -> str | None:
+        sizeX = self.sizeX
+        sizeY = self.sizeY
+        if 0 <= x < sizeX and 0 <= y < sizeY:
+            return self.char[x * sizeY + y]
+        return None
+
+    @property
+    def sizeX(self) -> int:
+        return self._sizeX
+
+    @property
+    def sizeY(self) -> int:
+        return self._sizeY
+
+    @property
+    def shouldRerender(self) -> bool:
+        return self.dirty
+
+    def iterAttributes(self) -> Iterator[int]:
+        fg = self.fg
+        bg = self.bg
+        sizeY = self.sizeY
+        for x in range(self.sizeX):
+            for y in range(sizeY):
+                pos = x * sizeY + y
+                yield mix_color(fg[pos], bg[pos])
+
+
+class WinRender(Renderer):
+    def __init__(self):
+        super().__init__()
+        self.buffer: Optional[PyConsoleScreenBufferType] = None
+        self.needRegen = True
+        self.last: Optional[WinCanvas] = None
+
+    def initialize(self):
+        pass
+
+    def onResize(self):
+        self.needRegen = True
+
+    def render(self, canvas: Canvas):
+        if isinstance(canvas, WinCanvas):
+            buf = self.buffer
+            buf.WriteConsoleOutputCharacter(
+                canvas.char, PyCOORDType(0, 0)
+            )
+            buf.WriteConsoleOutputAttribute(
+                canvas.iterAttributes(), PyCOORDType(0, 0)
+            )
+
+    def regenCanvas(self) -> WinCanvas:
+        buf = win32console.CreateConsoleScreenBuffer(
+            DesiredAccess=win32con.GENERIC_READ | win32con.GENERIC_WRITE,
+            ShareMode=0, SecurityAttributes=None, Flags=1)
+        self.buffer = buf
+        buf.SetConsoleActiveScreenBuffer()
+        info = buf.GetConsoleScreenBufferInfo()
+        size = info["Size"]
+        width, height = size.X, size.Y
+        c = WinCanvas(width, height)
+        c.char = np.full(width * height, " ", dtype=str)
+        c.fg = np.full(width * height, 0, dtype=int)
+        c.bg = np.full(width * height, 0, dtype=int)
+        return c
+
+    def getCanvas(self) -> WinCanvas:
+        canvas = self.last
+        if canvas is None or self.needRegen:
+            canvas = self.regenCanvas()
+            self.needRegen = False
+        return canvas
+
+    def dispose(self):
+        self.buffer.Close()
